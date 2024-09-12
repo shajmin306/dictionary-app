a85de7a914b889732c091d1da4e421973c6e5b3f
shajmin306 <157534142+shajmin306@users.noreply.github.com>
2024-09-12 21:55:29 +0100

    Added Search Form to dictionary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header text-center">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Shajmin Rahman</footer>
+      </div>
     </div>
   );
 }
