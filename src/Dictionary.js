772b8f77c079b3582c1082fa664a6552b904dad0
shajmin306 <157534142+shajmin306@users.noreply.github.com>
2024-09-29 12:19:15 +0100

    Added photos to app

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "Ssk8v9Oyj7NYaDLSRjqVEarRjIXCbF32l44ECPSwzQlsycgFa5XZ897v";
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    axios
+      .get(pexelsUrl, {
+        headers: {
+          Authorization: `
+         ${pexelsApiKey}`,
+        },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -33,6 +51,7 @@ export default function Dictionary() {
           />
         </form>
         <Results results={results} />
+        <Photos photos={photos} />
       </section>
     </div>
   );
---
+++ src/Photos.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photos, index) {
+            return (
+              <div className="col-4" key={index}>
+                <img src={photos.src.tiny} className="img-fluid" />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
